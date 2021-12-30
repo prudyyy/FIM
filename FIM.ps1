1e3779cadd043b242465f5f94e1311d5d08003d7
Ilyas Fakhor <47326490+prudyyy@users.noreply.github.com>
2021-12-30 14:21:50 +0100

    Add files via upload

---
+++ FIM.ps1
@@ -0,0 +1,93 @@
+﻿#Hello this is my iteration of a FIM (File Integration Monitor), I'll try to keep updating it and optimazing it if possible.
+#If you have any suggestions feel free to reach to me, Thank you.
+#If the script doesnt work just change the files path to where the files that you want to monitor are located
+
+
+Function Calculate-File-Hash($filepath) {
+    $filehash = Get-FileHash -Path $filepath -Algorithm SHA512
+    return $filehash
+}
+
+Function Erase-Baseline-If-Already-Exists() {
+    $baselineExists = Test-Path -Path .\Documents\baseline.txt
+
+    if ($baselineExists) {
+        # Delete it
+        Remove-Item -Path .\Documents\baseline.txt
+    }
+}
+ 
+Write-Host ""
+Write-Host "|------------------------File Integrity Monitor------------------------| "
+Write-Host "|                                                                      |"
+Write-Host "|     A) Collect new Baseline?                                         |"
+Write-Host "|    B) Begin monitoring files with saved Baseline?                    |"
+Write-Host "|                                                                      |"
+Write-Host "|----------------------------------------------------------------------|"
+$response = Read-Host -Prompt "           Please enter 'A' or 'B'"
+Write-Host ""
+
+if ($response -eq "A".ToUpper()) {
+    # Delete baseline.txt if it already exists
+    Erase-Baseline-If-Already-Exists
+
+    # Calculate Hash from the target files and store in baseline.txt
+    # Collect all files in the target folder
+    $files = Get-ChildItem -Path .\Documents\Files
+     $files
+    # For each file, calculate the hash, and write to baseline.txt
+    foreach ($f in $files) {
+        $hash = Calculate-File-Hash $f.FullName
+        "$($hash.Path)|$($hash.Hash)" | Out-File -FilePath .\Documents\baseline.txt -Append
+    }
+    
+}
+elseif ($response -eq "B".ToUpper()) {
+    
+    $fileHashDictionary = @{}
+
+    # Load file|hash from baseline.txt and store them in a dictionary
+    $filePathsAndHashes = Get-Content -Path .\Documents\baseline.txt
+    
+    foreach ($f in $filePathsAndHashes) {
+         $fileHashDictionary.add($f.Split("|")[0],$f.Split("|")[1])
+    }
+
+    # Begin (continuously) monitoring files with saved Baseline
+    while ($true) {
+        Start-Sleep -Seconds 1
+        
+        $files = Get-ChildItem -Path .\Documents\Files
+
+        # For each file, calculate the hash, and write to baseline.txt
+        foreach ($f in $files) {
+            $hash = Calculate-File-Hash $f.FullName
+            #"$($hash.Path)|$($hash.Hash)" | Out-File -FilePath .\baseline.txt -Append
+
+            # Notify if a new file has been created
+            if ($fileHashDictionary[$hash.Path] -eq $null) {
+                # A new file has been created!
+                Write-Host "$($hash.Path) has been created!" -ForegroundColor Green
+            }
+            else {
+
+                # Notify if a new file has been changed
+                if ($fileHashDictionary[$hash.Path] -eq $hash.Hash) {
+                    # The file has not changed
+                }
+                else {
+                    # File file has been compromised!, notify the user
+                    Write-Host "$($hash.Path) has changed!!!" -ForegroundColor Yellow
+                }
+            }
+        }
+
+        foreach ($key in $fileHashDictionary.Keys) {
+            $baselineFileStillExists = Test-Path -Path $key
+            if (-Not $baselineFileStillExists) {
+                # One of the baseline files must have been deleted, notify the user
+                Write-Host "$($key) has been deleted!" -ForegroundColor DarkRed -BackgroundColor Gray
+            }
+        }
+    }
+}
